d30dacfd569835b69e23e6284662b213dabddeed
Frederik Haagensen <frehaa@hotmail.dk>
2017-08-12 12:06:35 +0200

    Add test for type behavior

---
+++ CSharpTests/TypesStuff.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSharpTests
+{
+    [TestClass]
+    public class TypesStuff
+    {
+        [TestMethod]
+        public void IntComparedToUint()
+        {
+            int left = 1;
+            uint right = 1;
+            Assert.IsTrue(left == right);
+            Assert.IsFalse(left.Equals(right));
+        }
+    }
+}
