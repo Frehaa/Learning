a2a314cd0d130cafeeef927a204d2482e069adda
Frederik Haagensen <frehaa@hotmail.dk>
2017-08-12 14:27:21 +0200

    Learn about mouse hooks

--- WINAPI/Program.cs
+++ WINAPI/Program.cs
@@ -11,6 +11,14 @@ namespace Learning.WINAPI
 {
     class Program
     {
+        public delegate int HookProc(int code, IntPtr wParam, IntPtr lParam);
+
+        private static HookProc keyboardHookProcedure = LowLevelKeyboardHookCallback;
+        private static HookProc mouseHookProcedure = LowLevelMouseHookCallback;
+
+        private static IntPtr keyboardHookId;
+        private static IntPtr mouseHookId;
+
         public enum HookType
         {
             WH_MIN = (-1),                          
@@ -51,10 +59,51 @@ namespace Learning.WINAPI
             LLKHF_UP = 0x80,
         }
 
-        private const int WM_KEYDOWN = 0x0100;
-        private const int WM_KEYUP = 0x0101;
-        private const int WM_SYSKEYDOWN = 0x0104;
-        private const int WM_SYSKEYUP = 0x0105;
+        [Flags]
+        public enum KeyFlags
+        {
+            WM_KEYDOWN = 0x0100,
+            WM_KEYUP = 0x0101,
+            WM_SYSKEYDOWN = 0x0104,
+            WM_SYSKEYUP = 0x0105
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT
+        {
+            public int x;
+            public int y;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct MouseStruct
+        {
+            public POINT point;
+            public uint mouseData;
+            public uint flags;
+            public uint time;
+            public UIntPtr dwExtraInfo;
+        }
+
+        [Flags]
+        public enum MouseFlags
+        {
+            WM_LBUTTONDOWN = 0x0201,
+            WM_LBUTTONUP = 0x0202,
+            WM_MOUSEMOVE = 0x0200,
+            WM_MOUSEWHEEL = 0x020A,
+            WM_MOUSEHWHEEL = 0x020E,
+            WM_RBUTTONDOWN = 0x0204,
+            WM_RBUTTONUP = 0x0205,
+            WM_XBUTTONDOWN = 0x020B,
+            WM_XBUTTONUP = 0x020C,
+            WM_XBUTTONDBLCLK = 0x020D,
+            WM_NCXBUTTONDOWN = 0x00AB,
+            WM_NCXBUTTONUP = 0x00AC,
+            WM_NCXBUTTONDBLCLK = 0x00AD,
+            WM_MBUTTONDOWN = 0x0207,
+            WM_MBUTTONUP = 0x0208,
+        }
 
 
         static void Main(string[] args)
@@ -64,54 +113,57 @@ namespace Learning.WINAPI
             {
                 IntPtr hModule = GetModuleHandle(module.ModuleName);
 
-                hookId = SetWindowsHookEx(HookType.WH_KEYBOARD_LL, hookProc, hModule, 0);
+                keyboardHookId = SetWindowsHookEx(HookType.WH_KEYBOARD_LL, keyboardHookProcedure, hModule, 0);
+                mouseHookId = SetWindowsHookEx(HookType.WH_MOUSE_LL, mouseHookProcedure, hModule, 0);
+
                 Application.Run(new Form());
 
-                if (UnhookWindowsHookEx(hookId))
+                if (UnhookWindowsHookEx(keyboardHookId))
                 {
                     Debug.WriteLine("Failed to unhook");
                     Debug.WriteLine("Error code: " + Marshal.GetLastWin32Error());
                 }
-                
+
+                if (UnhookWindowsHookEx(mouseHookId))
+                {
+                    Debug.WriteLine("Failed to unhook");
+                    Debug.WriteLine("Error code: " + Marshal.GetLastWin32Error());
+                }
+
             }
 
         }
-
-        public delegate int HookProc(int code, IntPtr wParam, IntPtr lParam);
-
-        private static HookProc hookProc = MyCallBack;
-        private static IntPtr hookId;
-
-        private static int MyCallBack(int code, IntPtr wParam, IntPtr lParam)
+        
+        private static int LowLevelKeyboardHookCallback(int code, IntPtr wParam, IntPtr lParam)
         {
             if (code < 0)
             {
                 return CallNextHookEx(IntPtr.Zero, code, wParam, lParam);
             }
 
-            int keyboardMessageIdentifier = wParam.ToInt32();
-
+            KeyFlags keyboardMessageIdentifier = (KeyFlags)wParam.ToInt32();
+            
             switch (keyboardMessageIdentifier)
             {
-                case WM_KEYDOWN:
+                case KeyFlags.WM_KEYDOWN:
                     Debug.WriteLine("WM_KEYDOWN");
                     break;
-                case WM_KEYUP:
+                case KeyFlags.WM_KEYUP:
                     Debug.WriteLine("WM_KEYUP");
                     break;
-                case WM_SYSKEYDOWN:
+                case KeyFlags.WM_SYSKEYDOWN:
                     Debug.WriteLine("WM_SYSKEYDOWN");
                     break;
-                case WM_SYSKEYUP:
+                case KeyFlags.WM_SYSKEYUP:
                     Debug.WriteLine("WM_SYSKEYUP");
                     break;
                 default:
-                    Debug.Fail("ERROR: UNKNOWN KEY");
+                    Debug.Fail("Unknown flag: " + keyboardMessageIdentifier);
                     break;
-            }           
+            }
             
-            KeyboardStruct kbs = (KeyboardStruct)Marshal.PtrToStructure(lParam, typeof(KeyboardStruct));
-
+            KeyboardStruct kbs = Marshal.PtrToStructure<KeyboardStruct>(lParam);
+            
             Keys key = (Keys)kbs.vkCode;
             Debug.WriteLine(key);
             Debug.WriteLine(kbs.scanCode);
@@ -119,6 +171,78 @@ namespace Learning.WINAPI
             Debug.WriteLine(kbs.time);
             Debug.WriteLine(kbs.dwExtraInfo.ToUInt64());
 
+            Debug.WriteLine("");
+
+            return CallNextHookEx(IntPtr.Zero, code, wParam, lParam);
+        }
+
+        private static int LowLevelMouseHookCallback(int code, IntPtr wParam, IntPtr lParam)
+        {
+            if (code < 0)
+            {
+                return CallNextHookEx(IntPtr.Zero, code, wParam, lParam);
+            }
+
+            MouseFlags flag = (MouseFlags)wParam;
+            switch (flag)
+            {
+                case MouseFlags.WM_LBUTTONDOWN:
+                    Debug.WriteLine(MouseFlags.WM_LBUTTONDOWN);
+                    break;
+                case MouseFlags.WM_LBUTTONUP:
+                    Debug.WriteLine(MouseFlags.WM_LBUTTONUP);
+                    break;
+                case MouseFlags.WM_MOUSEMOVE:
+                    Debug.WriteLine(MouseFlags.WM_MOUSEMOVE);
+                    break;
+                case MouseFlags.WM_MOUSEWHEEL:
+                    Debug.WriteLine(MouseFlags.WM_MOUSEWHEEL);
+                    break;
+                case MouseFlags.WM_MOUSEHWHEEL:
+                    Debug.WriteLine(MouseFlags.WM_MOUSEHWHEEL);
+                    break;
+                case MouseFlags.WM_RBUTTONDOWN:
+                    Debug.WriteLine(MouseFlags.WM_RBUTTONDOWN);
+                    break;
+                case MouseFlags.WM_RBUTTONUP:
+                    Debug.WriteLine(MouseFlags.WM_RBUTTONUP);
+                    break;
+                case MouseFlags.WM_XBUTTONDOWN:
+                    Debug.WriteLine(MouseFlags.WM_XBUTTONDOWN);
+                    break;
+                case MouseFlags.WM_XBUTTONUP:
+                    Debug.WriteLine(MouseFlags.WM_XBUTTONUP);
+                    break;
+                case MouseFlags.WM_XBUTTONDBLCLK:
+                    Debug.WriteLine(MouseFlags.WM_XBUTTONDBLCLK);
+                    break;
+                case MouseFlags.WM_NCXBUTTONDOWN:
+                    Debug.WriteLine(MouseFlags.WM_NCXBUTTONDOWN);
+                    break;
+                case MouseFlags.WM_NCXBUTTONUP:
+                    Debug.WriteLine(MouseFlags.WM_NCXBUTTONUP);
+                    break;
+                case MouseFlags.WM_NCXBUTTONDBLCLK:
+                    Debug.WriteLine(MouseFlags.WM_NCXBUTTONDBLCLK);
+                    break;
+                case MouseFlags.WM_MBUTTONDOWN:
+                    Debug.WriteLine(MouseFlags.WM_NCXBUTTONDBLCLK);
+                    break;
+                case MouseFlags.WM_MBUTTONUP:
+                    Debug.WriteLine(MouseFlags.WM_MBUTTONUP);
+                    break;
+                default:
+                    Debug.WriteLine("Unknown Flag: {0:X}", flag);
+                    break;
+            }
+
+            MouseStruct mouseStruct = Marshal.PtrToStructure<MouseStruct>(lParam);
+            Debug.WriteLine(mouseStruct.point.x + ", " + mouseStruct.point.y);
+            Debug.WriteLine(mouseStruct.mouseData);
+            Debug.WriteLine(mouseStruct.flags);            
+            //Debug.WriteLine(mouseStruct.time);
+            if (mouseStruct.dwExtraInfo.ToUInt32() != 0)
+                Debug.WriteLine(mouseStruct.dwExtraInfo);
             Debug.WriteLine("");
 
             return CallNextHookEx(IntPtr.Zero, code, wParam, lParam);
