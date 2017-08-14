927f476787e7e96349179b0f65cfdf49e43ad71b
Frederik Haagensen <frehaa@hotmail.dk>
2017-08-14 13:21:12 +0200

    Learn about field initialization of delegates

---
+++ CSharp/DelegateClass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    class DelegateClass
+    {
+        private delegate void MyDelegate();
+
+        // You can field initialize a delegate if the method is static
+        private MyDelegate myDelegateReference = MyStaticDelegateMethod;
+
+        // You can't field initialize if the method is non-static
+        //private MyDelegate myDelegateReference = MyNonStaticDelegateMethod;
+
+
+        public DelegateClass()
+        {
+            // In the constructor you can initialize both static and non-static
+            myDelegateReference = MyNonStaticDelegateMethod;
+            myDelegateReference = MyStaticDelegateMethod;
+        }
+
+
+        public void MyNonStaticDelegateMethod()
+        {
+
+        }
+        
+        public static void MyStaticDelegateMethod()
+        {
+
+        }
+    }
+}
