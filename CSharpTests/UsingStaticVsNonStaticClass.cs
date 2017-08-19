e2051f2a0e5ea615530eebfa8cfabdf4339b0d6b
Frederik Haagensen <frehaa@hotmail.dk>
2017-08-19 10:19:30 +0200

    Added learning test for using static methods in class with using statements

---
+++ CSharpTests/UsingStaticVsNonStaticClass.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static CSharpTests.StaticClass;
+using static CSharpTests.NonStaticClass;
+
+namespace CSharpTests
+{
+    [TestClass]
+    public class UsingStaticVsNonStaticClass
+    {
+        [TestMethod]
+        public void UsingStaticClassMethod()
+        {
+            Assert.AreEqual(true, StaticClassStaticMethod());
+        }
+
+        [TestMethod]
+        public void UsingNonStaticClassMethod()
+        {
+            Assert.AreEqual(true, NonStaticClassStaticMethod());
+        }
+    }
+
+    static class StaticClass
+    {
+        public static bool StaticClassStaticMethod()
+        {
+            return true;
+        }
+    }
+
+    class NonStaticClass
+    {
+        public static bool NonStaticClassStaticMethod()
+        {
+            return true;
+        }
+    }
+}
