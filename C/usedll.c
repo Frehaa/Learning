e23c808dafb00731c8b261e02f6340f783b8a607
Frederik Haagensen <haag@itu.dk>
2022-09-01 20:50:47 +0200

    Try weird cast in C

--- C/usedll.c
+++ C/usedll.c
@@ -31,71 +31,71 @@ typedef struct {
 int main(int argc, char* argv) {
 // int WinMain(HINSTANCE instance, HINSTANCE prevInstance, LPSTR commandLine, int showCode){
 
-    Cow betty;
-    betty.name = "Betty";
-    betty.age = 12;
-    betty.type = 1;
+    // Cow betty;
+    // betty.name = "Betty";
+    // betty.age = 12;
+    // betty.type = 1;
 
-    int32_t t = ('a' << 24) + ('b' << 16) + ('c' << 8) + 'd';
-    char *test = (char*) &t;
-    printf("t = %d\n", t);
-    printf(test);
+    // int32_t t = ('a' << 24) + ('b' << 16) + ('c' << 8) + 'd';
+    // char *test = (char*) &t;
+    // printf("t = %d\n", t);
+    // printf(test);
 
     // This does not work for some reason :'<
     // Cat test2 = (Cat) betty;
 
-    // printf("Write name of dll to load: ");
+    printf("Write name of dll to load: ");
 
-    // char inbuf[50] = {0};
-    // int c;
-    // for (size_t i = 0; i < 40; i++)
-    // {
-    //     c = fgetc(stdin);
-    //     if (c == 10) break;
-    //     inbuf[i] = c;
-    // }
+    char inbuf[50] = {0};
+    int c;
+    for (size_t i = 0; i < 40; i++)
+    {
+        c = fgetc(stdin);
+        if (c == 10) break;
+        inbuf[i] = c;
+    }
     
-    // // HANDLE STDIN = GetStdHandle(STD_INPUT_HANDLE);
-    // // int bytesRead = _read((int)STDIN, inbuf, 40);
-    // // if (bytesRead <= 0) {
-    // //     printf("Problem reading");
-    // //     return 0;
-    // // }
-
-    // HMODULE add2dll = LoadLibraryA(inbuf);
-    // if (add2dll == NULL) {
-    //     printf("Failed to load dll: ");
-    //     printf(inbuf);
-    //     // OutputDebugStringA("Failed to load dll");
+    // HANDLE STDIN = GetStdHandle(STD_INPUT_HANDLE);
+    // int bytesRead = _read((int)STDIN, inbuf, 40);
+    // if (bytesRead <= 0) {
+    //     printf("Problem reading");
     //     return 0;
     // }
-    // printf("\nSuccessfully loaded ");
-    // printf(inbuf);
 
-    // printf("\nWrite name of procedure to load: ");
-    // char inbufproc[50] = {0};
-    // for (size_t i = 0; i < 40; i++)
-    // {
-    //     c = fgetc(stdin);
-    //     if (c == 10) break;
-    //     inbuf[i] = c;
-    // }
+    HMODULE add2dll = LoadLibraryA(inbuf);
+    if (add2dll == NULL) {
+        printf("Failed to load dll: ");
+        printf(inbuf);
+        // OutputDebugStringA("Failed to load dll");
+        return 0;
+    }
+    printf("\nSuccessfully loaded ");
+    printf(inbuf);
 
-    // IntToInt *proc = (IntToInt *)GetProcAddress(add2dll, "add2");
-    // if (proc == NULL) {
-    //     printf("Failed to find procedure: ");
-    //     printf(inbufproc);
-    //     // OutputDebugStringA("Failed to find procedure");
-    //     return 0;
-    // }
-    // printf("\nSuccessfully loaded ");
-    // printf(inbufproc);
+    printf("\nWrite name of procedure to load: ");
+    char inbufproc[50] = {0};
+    for (size_t i = 0; i < 40; i++)
+    {
+        c = fgetc(stdin);
+        if (c == 10) break;
+        inbuf[i] = c;
+    }
 
-    // int x = 4;
+    IntToInt *proc = (IntToInt *)GetProcAddress(add2dll, "add2");
+    if (proc == NULL) {
+        printf("Failed to find procedure: ");
+        printf(inbufproc);
+        // OutputDebugStringA("Failed to find procedure");
+        return 0;
+    }
+    printf("\nSuccessfully loaded ");
+    printf(inbufproc);
 
-    // char buf[50];
-    // sprintf(buf, "x = %d - proc(x) = %d", x, proc(x));
-    // printf(buf);
-    // // OutputDebugStringA(buf);
-    // return 0;
+    int x = 4;
+
+    char buf[50];
+    sprintf(buf, "x = %d - proc(x) = %d", x, proc(x));
+    printf(buf);
+    // OutputDebugStringA(buf);
+    return 0;
 }
